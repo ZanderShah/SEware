b7608d136f851a5c6bfeaeef88f9b7be27d30524
Tiger Zhao <tigerzhao.zhao@gmail.com>
2016-11-20 15:52:13 -0500

    merge conflicts

--- Level.ino
+++ Level.ino
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-void Reset(GameState *gs) {
-  free(gs->words);
-  gs->words = NULL;
-  free(gs->shapes);
-  gs->shapes = NULL;
-  gs->needsReset = 1;
-}
-
-void SetMemory(GameState *gs, int words, int shapes) {
-  gs->numWords = words;
-  gs->words = (Word*) malloc(sizeof(Word) * words);
-  gs->numShapes = shapes;
-  gs->shapes = (Shape*) malloc(sizeof(Shape) * shapes);
-}
-=======
 void Sync(OrbitInput *obi, GameState *gs) {
   for (int i = 0; i < BUTTON_COUNT; i++) {
     if (obi->buttons[i] && gs->buttonCooldown[i] == 0)
@@ -130,4 +114,3 @@ void UpdatePosition(Position *p){
 
 
 
->>>>>>> 09c42cad74df9c4a99d9b354b4664b8574d44849
